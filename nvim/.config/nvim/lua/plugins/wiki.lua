c8086f4499b924ec6f56aeff88fcc1bb0fa7807a
Ross Hartley <rossh@nautilus.bio>
2026-03-03 15:26:07 -0800

    Cleanup plugins

--- nvim/.config/nvim/lua/plugins/wiki.lua
+++ nvim/.config/nvim/lua/plugins/wiki.lua
@@ -1,12 +1,4 @@
 return {
-  -- {
-  --   "lervag/wiki.vim",
-  --   -- tag = "v0.10", -- uncomment to pin to a specific release
-  --   init = function()
-  --     -- wiki.vim configuration goes here, e.g.
-  --     vim.g.wiki_root = "~/wiki"
-  --   end,
-  -- },
   {
     "vimwiki/vimwiki",
     init = function()
